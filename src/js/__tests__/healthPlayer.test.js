5e2361779e1567a96d520b3ea29c3e462c15ae39
Maksim <yaschyk.maksim@yandex.ru>
2026-06-20 12:16:48 +0300

    Modified .gitignore

--- src/js/__tests__/healthPlayer.test.js
+++ src/js/__tests__/healthPlayer.test.js
@@ -1,14 +1,14 @@
 import healthPlayer from '../../script';
 
 test('should healthy', () => {
-  const result = healthPlayer({ name: 'Маг', health: 90 });
+  const result = healthPlayer({name: 'Маг', health: 90});
   expect(result).toBe('healthy');
 });
 test('should wounded', () => {
-  const result = healthPlayer({ name: 'Маг', health: 90 });
+  const result = healthPlayer({name: 'Маг', health: 30});
   expect(result).toBe('wounded');
 });
 test('should critical', () => {
-  const result = healthPlayer({ name: 'Маг', health: 90 });
+  const result = healthPlayer({name: 'Маг', health: 10});
   expect(result).toBe('critical');
 });
--- src/script.js
+++ src/script.js
@@ -1,8 +1,7 @@
 export default function healthPlayer(player) {
   if (player.health > 50) {
     return 'healthy';
-  }
-  if (player.health <= 50 && player.health >= 15) {
+  } if (player.health <= 50 && player.health >= 15) {
     return 'wounded';
   }
   return 'critical';
